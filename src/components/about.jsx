46151823d8e72beec9d55f6164657e863cdd1d2d
kevinlin95 <contact.kevinlin95@gmail.com>
2022-06-05 08:25:22 -0400

    1

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am a computer science student from Queens, New York. I love exploring new technologies and getting lost in the sea of new terminology and concepts. Learning is an eternal goal for me and I strive to learn something new everyday!</p>
+                    <p>I am currently a Developer working at GenSpark, learning the development workflow and adding an arsenal of programming/db tools under my skillset. I hope to one day develop my skills enough to be able to share my knowledge with others.</p>
                     </div>
                 </div>
                 </div>
@@ -60,7 +60,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <p>I am pursuing my internship with DevOps team at GenSpark and working with tools like SpringBoot, MySQL, Postman, AWSCloud</p>
                 </div>
                 </div>
             </div>
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,8 +21,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
+                        <h2>Internship at GenSpark <span>2022-present</span></h2>
+                        <p>I recently have joined GenSpark as a Junior Java Developer-Intern in the DevOps team. My duties included working on projects and building user interfaces for GenSpark clients.</p>
                       </div>
                     </div>
                   </article>
@@ -32,8 +32,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                        <h2>Undergrad at LaGuardia Community College<span>2020-2022</span></h2>
+                        <p>I am pursuing my under-graduation studies with major in Computer Science. Graduated with a GPA of 3.2 and will be moving on to higher education. </p>
                       </div>
                     </div>
                   </article>
@@ -43,8 +43,8 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <h2>Career in the Food Industry <span>2015-2020</span></h2>
+                        <p>I started off working for my pops at his Chinese Food takeout making fried rice and chicken wings before moving on to being a busser at a fine dining establishment. Eventually I would move back into cooking and work in Japanese, Korean and Chinese cuisines.</p> 
                       </div>
                     </div>
                   </article>
