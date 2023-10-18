29800c21668230bf1cdd762c70956fd8466af666
kevinlin <contact.kevinlin95@gmail.com>
2023-10-18 01:49:46 -0400

    10/18 new update

---
+++ src/components/about.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a computer science student from Queens, New York. I love exploring new technologies and getting lost in the sea of new terminology and concepts. Learning is an eternal goal for me and I strive to learn something new everyday!</p>
+                    <p>I am currently a web/software developer working at CUNY LaGuardia Community College, learning the development workflow and adding an arsenal of programming/db tools under my skill-set. I hope to one day develop my skills enough to be able to share my knowledge with others.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development & CRUD/API's </h3>
+                    <p>I have some experience building websites using JavaScript, React, HTML/CSS. I have also worked on backend projects writing API's and CRUD methods using Node, Java, Spring Boot.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Structures & Algorithms</h3>
+                    <p>As coming a CS background, I have good grasp over fundamental concepts of DSA.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Leadership and Teamwork</h3>
+                    <p>After spending several years in the food and hospitality industry has given me great insight in how to effectively communicate with others and prioritize necessary tasks.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergrad at CUNY Queens College <span> Sep 2023 - June 2025</span></h2>
+                        <p>I am finishing my undergraduate degree in Computer Science and looking for more internship opportunities</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at CUNY SOAR/STORE IT <span> April 2023 - Sep 2023</span></h2>
+                        <p>I recently have joined the IT Department at my school as a web developer. I am working on their WordPress CMS and using tools like Adobe Creative Suite.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Goldman Sachs 10000 Small Businesses - StemKASA <span> Jan 2023 - May 2023</span></h2>
+                        <p>I recently have joined StemKASA as a web developer. My daily tasks include writing and reviewing code, and collaborating with my cohorts.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at GenSpark<span> March 2022 - Sep 2022</span></h2>
+                        <p>I recently have joined GenSpark as a Junior Java Developer-Intern in the Software Development team. My duties included working on projects and building user interfaces for GenSpark clients.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergrad at CUNY LaGuardia Community College <span> Sep 2020 - June 2023</span></h2>
+                        <p>I am pursuing my under-graduation studies with major in Computer Science. Graduated with a GPA of 3.2 and will be moving on to higher education.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Career in the Food and Hospitality Industry <span> 2015 - 2020</span></h2>
+                        <p>I started off working for my pops at his Chinese food restaurant. I also helped out as a manager for his laundromat shop learning to effectively lead in both businesses. Eventually I would move back into cooking and work in Japanese, Korean and Chinese cuisines.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
