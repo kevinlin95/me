9bef587203c535d29b4f652d59108a70bc8ff5f1
kevinlin <contact.kevinlin95@gmail.com>
2023-10-18 02:18:11 -0400

    update background image

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,6 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -20,6 +21,8 @@ export default class Introduction extends Component {
                     </div>
                   </div>
                 </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -33,6 +36,8 @@ export default class Introduction extends Component {
                     </div>
                   </div>
                 </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -46,6 +51,7 @@ export default class Introduction extends Component {
                     </div>
                   </div>
                 </div>
+              </li>
             </ul>
           </div>
         </section>
